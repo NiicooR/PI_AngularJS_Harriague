31799e002e5cf6956408f2ab6e3c22dcb63fae50
NiicooR <nicoramirez.isi@gmail.com>
2016-12-10 12:49:03 -0300

    yaoman ready

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name freeReviewsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the freeReviewsApp
+ */
+angular.module('freeReviewsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
