9a9af6484d8eda9f412d90e0819462412061b5a2
NiicooR <nicoramirez.isi@gmail.com>
2016-12-10 23:46:49 -0300

    part of main.js done

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,79 @@
  * # MainCtrl
  * Controller of the freeReviewsApp
  */
-angular.module('freeReviewsApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+ angular.module('freeReviewsApp')
+ .service('MainInfoService', MainInfoService) 
+ .constant('BDBasePath', '../../BD/generated.json')
+ .constant('CategoriesBasePath', '../../BD/categories.json')
+.controller('MainCtrl', MainCtrl)
+
+  MainCtrl.$inject = ['MainInfoService'];
+  function MainCtrl(MainInfoService){
+  	var main = this;
+
+  	var promise = MainInfoService.getMainInfo();
+
+	promise.then(function(response){
+  		main.services = response.data;
+  	})
+  	.catch(function(error){
+  		console.log("Something went terribly wrong, looking for services");
+ 	});
+
+  
+  	main.logCategories = function(){
+  		var promise = MainInfoService.getCategories();
+
+    promise.then(function (response) {
+    	main.categories = response.data; 
+      
+    })
+    .catch(function (error) {
+      console.log("Something went terribly wrong, looking for categories");
+    })
+  	}
+  	
+
+  }
+
+
+MainInfoService.$inject = ['$http', 'BDBasePath', 'CategoriesBasePath'];
+function MainInfoService($http, BDBasePath, CategoriesBasePath ){
+	var service = this;
+
+	service.getMainInfo = function(){
+		var response = $http({
+      method: "GET",
+      url: (BDBasePath)
+    });
+		return response;
+	};
+
+ 	service.getCategories = function(){
+ 		var response = $http({
+      method: "GET",
+      url: (CategoriesBasePath)
+    });
+		return response;
+	};
+ 	
+
+
+
+  };
+
+
+
+
+
+
+
+
+
+//.controller('MainCtrl', function () {
+// 	this.awesomeThings = [
+//	'HTML5 Boilerplate',
+// 	'AngularJS',
+//	'Karma'
+// 	];
+// });
